44619529e7b9408d7faf457bd45e1d3b11b68349
Alexander Bersenev <bay@hackerdom.ru>
2017-09-29 18:36:47 +0500

    tests

---
+++ tests.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import unittest
+import suggest
+
+
+class TestSuggester(unittest.TestCase):
+
+    def test_none(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
